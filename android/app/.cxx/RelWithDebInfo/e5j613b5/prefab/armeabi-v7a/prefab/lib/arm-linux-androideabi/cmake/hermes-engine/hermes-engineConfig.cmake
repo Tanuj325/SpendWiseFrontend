b732a9b80bf772ecc87edf8b957d95b0fcd09365
Tanuj Pratap Singh <tanujsinghthakur9999@gmail.com>
2026-05-28 13:34:23 +0530

    cleanup build and update gitignore

---
+++ android/app/.cxx/RelWithDebInfo/e5j613b5/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/hermes-engine/hermes-engineConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET hermes-engine::hermesvm)
+add_library(hermes-engine::hermesvm SHARED IMPORTED)
+set_target_properties(hermes-engine::hermesvm PROPERTIES
+    IMPORTED_LOCATION "C:/Users/tanuj/.gradle/caches/8.13/transforms/ea8bafcafc2c6c325031eaffe24fde9a/transformed/hermes-android-0.14.0-release/prefab/modules/hermesvm/libs/android.armeabi-v7a/libhermesvm.so"
+    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/tanuj/.gradle/caches/8.13/transforms/ea8bafcafc2c6c325031eaffe24fde9a/transformed/hermes-android-0.14.0-release/prefab/modules/hermesvm/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
---
+++ android/app/.cxx/RelWithDebInfo/e5j613b5/prefab/x86/prefab/lib/i686-linux-android/cmake/react-native-reanimated/react-native-reanimatedConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET react-native-reanimated::reanimated)
+add_library(react-native-reanimated::reanimated SHARED IMPORTED)
+set_target_properties(react-native-reanimated::reanimated PROPERTIES
+    IMPORTED_LOCATION "D:/Tanuj/Mobile Development/SpendWise/node_modules/react-native-reanimated/android/build/intermediates/cxx/RelWithDebInfo/15413ac2/obj/x86/libreanimated.so"
+    INTERFACE_INCLUDE_DIRECTORIES "D:/Tanuj/Mobile Development/SpendWise/node_modules/react-native-reanimated/android/build/prefab-headers/reanimated"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
